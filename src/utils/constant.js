898940537404b0bf6d1eca145a2fa12ca03da913
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-06 23:54:52 +0530

    base update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+
+export const USER_API_END_POINT = "https://job-application-be.onrender.com/api/v1";
+export const JOB_API_END_POINT = "https://job-application-be.onrender.com/api/v1";
+export const APPLICATION_API_END_POINT =
+  "https://job-application-be.onrender.com/api/v1";
+export const COMPANY_API_END_POINT = "https://job-application-be.onrender.com/api/v1";
+
+
+/* export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
 export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"; */
